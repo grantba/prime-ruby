7d9977f902d45635fa08c909e42727e265857e08
Blaire <grant.blaire@gmail.com>
2020-11-20 22:12:50 -0600

    commit tree 5066b181031066e39d07121ebe43c93e4f9f6223

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+    lowest_number = 2
+    if number > 1
+        range = (lowest_number..number -1).to_a
+        range.none? do |num|
+            number % num == 0
+        end
+    else
+        false
+    end
+end
+
+
+#prime?(40)
+
+
